ae10e08a760cb163b23ba9eb79da1ce4d5c1a80f
Ahmed Soudy <ahmed.soudy30@yahoo.com>
2024-12-12 18:02:23 +0200

    nagwa-lastwork-6

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -35,7 +35,24 @@ def logout_user(request):   # My view can't be named login because it will confl
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user) 
+            messages.success(request, "You have successfully Registered!")
+            return redirect('home')
+    else:
+        form = SignUpForm()   # bas hena mafy4 request passed 34an homa mamlo4 el form, they want to fillout the form, they haven't done it yet
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
+
+
 
 
 
